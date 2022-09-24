615410fd0c384878a18008c48be82a38d62d896e
Wombatya <naduha.9612@gmail.com>
2022-09-24 15:08:06 +0300

    The second lesson

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-console.log('Hello world!');
+// console.log("Hello world!");
 
-const ourPlanetName = 'Earth';
-console.log(ourPlanetName);
+// const ourPlanetName = "Earth";
+// console.log(ourPlanetName);
 
-let currentUserTime = '12:03';
-console.log(currentUserTime);
+// let currentUserTime = "12:03";
+// console.log(currentUserTime);
 
-let articlesCount = 'No. Articles';
-console.log(articlesCount);
+// let articlesCount = "No. Articles";
+// console.log(articlesCount);
 
-let isCash = 'Cash or card?';
-console.log(isCash);
+// let isCash = "Cash or card?";
+// console.log(isCash);
 
-const userName = 'Nadezhda';
-console.log(userName);
+// const userName = "Nadezhda";
+// console.log(userName);
 
-const userSurname = 'Komarova';
-console.log(userSurname);
+// const userSurname = "Komarova";
+// console.log(userSurname);
 
-const userPatronymic = 'Viktorovna';
-console.log(userPatronymic);
+// const userPatronymic = "Viktorovna";
+// console.log(userPatronymic);
 
+// const age = 18;
+// const firstName = "N";
+// const userData = `Имя пользователя: ${firstName}, возраст: ${age}`;
+// console.log(userData);
+// //Это комментарий на одну строку. Его не видно в консоли.
+// /* Это комментарий на несколько строк,
+// его тоже не видно в консоли. */
+
+let UserData = {
+    name: "Ivan",
+    age: 34,
+};
+console.log(UserData.name);
+
+console.log(typeof UserData.age);
