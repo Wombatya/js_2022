2fedcdecbab2ccc86b786fa9bcf00103fa376388
Wombatya <naduha.9612@gmail.com>
2022-09-24 10:37:59 +0300

    First lesson files are added

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('Hello world!');
+
+const ourPlanetName = 'Earth';
+console.log(ourPlanetName);
+
+let currentUserTime = '12:03';
+console.log(currentUserTime);
+
+let articlesCount = 'No. Articles';
+console.log(articlesCount);
+
+let isCash = 'Cash or card?';
+console.log(isCash);
+
+const userName = 'Nadezhda';
+console.log(userName);
+
+const userSurname = 'Komarova';
+console.log(userSurname);
+
+const userPatronymic = 'Viktorovna';
+console.log(userPatronymic);
+
