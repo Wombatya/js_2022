b76c5cfb2e6701efab0e577b582afe160e60f485
John Doe <naduha.9612@gmail.com>
2022-10-01 15:23:56 +0300

    new lesson

---
+++ lesson 2.js
@@ -0,0 +1,21 @@
+// const name = "Alex";
+
+// const nameToNumber = Number(name);
+
+// console.log(nameToNumber);
+
+// const age = 26;
+
+// const ageToString = String(age);
+
+// console.log(typeof ageToString);
+
+// const nullValue = null;
+// console.log(Number(nullValue));
+
+// console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean({}));
+
+
---
+++ lesson2.js
@@ -0,0 +1,80 @@
+// const name = "Alex";
+
+// const nameToNumber = Number(name);
+
+// console.log(nameToNumber);
+
+// const age = 26;
+
+// const ageToString = String(age);
+
+// console.log(typeof ageToString);
+
+// const nullValue = null;
+// console.log(Number(nullValue));
+
+// console.log(Boolean(''));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean({}));
+
+// console.log(typeof +"45");
+// console.log(typeof Number("45"));
+
+// console.log(2 == 3);
+// console.log(2 == 2);
+// console.log(2 != 2);
+// console.log(2 < 3);
+// console.log(2 > 3);
+
+// console.log(5 > 4);
+// console.log("ананас" > "яблоко");
+// console.log("2" > "12");
+// console.log(undefined == null);
+// console.log(undefined === null);
+// console.log(null == "\n0\n");
+// console.log(null === +"\n0\n");
+// console.log("" + 1 + 0);
+// console.log("" - 1 + 0);
+// console.log(true + false);
+// console.log(6 / "3");
+// console.log("2" * "3");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 + 5);
+// console.log("4" - 2);
+// console.log("4px" - 2);
+// console.log(7 / 0);
+// console.log(" -9 " + 5);
+// console.log("-9" - 5);
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log("\t\n" - 2);
+
+// console.log(typeof {});
+// console.log(typeof []);
+
+// console.log(towns.length);
+
+// console.log(Array.isArray({}));
+// console.log(Array.isArray([]));
+
+// console.log(towns);
+// towns[1] = 'Milan';
+// console.log(towns);
+
+// towns[4]='St Petersburg';
+// towns[9] = 'Berlin';
+
+// console.log(towns.length);
+// console.log(towns);
+
+// const towns = ["Moscow", "Paris", "London", "NN"];
+// towns[1]='Milan';
+// towns.push('St Petersburg');
+// towns[towns.length] = 'Orel';
+// console.log(towns);
+
+const favFilms = ["Silent Hill", "Moonrise Kingdom", "Shutter Island"];
+console.log(favFilms[0]);
+favFilms.push('Fight Club');
+console.log(favFilms[favFilms.length-1]);
