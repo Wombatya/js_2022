06ffdba44d5e81b1d6337197adc85c360465a079
John Doe <naduha.9612@gmail.com>
2022-10-14 22:10:55 +0300

    Third lesson

---
+++ lesson_3.js
@@ -0,0 +1,145 @@
+// if(1) {
+//     console.log('true');
+// }
+// if('99') {
+//     console.log('false');
+// }
+
+// const name = "Alex3";
+// if (name === "Ivan") {
+//   console.log(Hello ${name});
+// } else if (name === "Alex") {
+//   console.log(Hello friend ${name});
+// } else if (name === "Petr") {
+//   console.log(Hi ${name});
+// } else {
+//   console.log("Hello stranger");
+// }
+
+// if("true") {
+//     if(1) {
+//         console.log('true');
+//     } else {
+//         console.log('false');}
+// } else {
+//     console.log('false');
+// }
+
+// const userName = 'Alex';
+
+// const result = userName === 'Alex' ? "Hello Alex" : "Hello user";
+// console.log(result);
+
+// const first = 30;
+// const second = 15;
+// const third = 20;
+
+// if (first > second && first > third) {
+//   console.log(The longest is ${first});
+// } else if (second > first && second > third) {
+//   console.log(The longest is ${second});
+// } else if (third > first && third > second) {
+//   console.log(The longest is ${third});
+// }
+
+// if (first < second) {
+//   if (second < third) {
+//     console.log(`The longest is ${third}`);
+//   } else {
+//     console.log(`The longest is ${second}`);
+//   }
+// } else if (first < third) {
+//   if (second < third) {
+//     console.log(`The longest is ${third}`);
+//   } else {
+//     console.log(`The longest is ${second}`);
+//   }
+// } else if (second < third) {
+//   if (third < first) {
+//     console.log(`The longest is ${first}`);
+//   } else {
+//     console.log(`The longest is ${third}`);
+//   }
+// } else {
+//     console.log(`The longest is ${first}`);
+// }
+
+// const userRole = "user";
+// switch (userRole) {
+//   case "admin":
+//     console.log("Birthday 20.03");
+//     break;
+//   case "manager":
+//     console.log("Birthday 14.05");
+//     break;
+//   case "user":
+//     console.log("Birthday 08.01");
+//     break;
+//   default:
+//     console.log("We don't know when you B-day is");
+// }
+
+// let temperature = 50
+
+// if (temperature <= -30) {
+//     console.log('Stay home!');
+// } else if (-30<temperature && temperature<= -10) {
+//     console.log("It's cold");
+// } else if (-10<temperature &&  temperature<= 5) {
+//     console.log("It isn't cold");
+// } else if (5<temperature && temperature <=15) {
+//     console.log("It's warm");
+// } else if (15<temperature && temperature<=25) {
+// console.log("It's really warm");
+// } else if (25<temperature && temperature<35) {
+//     console.log("It's hot");
+// } else if (temperature >= 35) {
+//     console.log("Scorching heat");
+// }
+
+// let i = 0;
+// while (i < 100) {
+//   console.log(i);
+//   i++;
+// }
+
+// const arr = [1, 3, 5, 6, 9, 12, 14, 16, 17, 18, 21, 25, 65, 37];
+
+// const result = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] % 2 !== 0) {
+//         result.push(arr[i]);
+//     }
+// } 
+
+// console.log(result)
+
+// let n = 5;
+// let i= 0;
+
+// // while (i<n) {
+// //     console.log("Hello!");
+// //     i++;
+// // }
+
+// for (;i<n; i++) {
+//     console.log("Hello");
+// }
+
+// let totalCarsNumber = 100;
+// let carsEveryDay = totalCarsNumber / 2;
+// let days = 1;
+// const finalNumber = 5;
+
+// while (carsEveryDay > finalNumber) {
+//     carsEveryDay = carsEveryDay / 2; 
+//     days++;
+// }
+// console.log(days);
+
+// let i=0;
+// for(let year = 1800; year<2022; year++) {
+//     if (year === 1961) {
+//         console.log(`The first journey into outer space ${year}, ${i}`);
+//     } else i++
+// }
